d1d90d043f2d2e9cf3a4e4f060449b0a1112811f
Freddy Rondon <freddy.rondon@mercadolibre.cl>
2017-07-17 14:55:22 -0400

    feat: add to model logic of created and get

--- addresses/model.go
+++ addresses/model.go
@@ -1,9 +1,52 @@
 package addresses
 
+import "database/sql"
+
 type Address struct {
+	ID      int     `json:"id"`
 	Address string  `json:"address"`
 	Lat     float32 `json:"lat"`
 	Lng     float32 `json:"lng"`
 }
 
+func (addrs *Address) getAddress(db *sql.DB) error {
+	return db.QueryRow("SELECT address, lat, lng FROM addresses WHERE id=$1", addrs.ID).Scan(&addrs.Address, &addrs.Lat, &addrs.Lng)
+}
+
+func (addrs *Address) createAddress(db *sql.DB) error {
+	err := db.QueryRow(
+		"INSERT INTO addresses(address, lat, lng) VALUES($1, $2, $3) RETURNING id",
+		addrs.Address, addrs.Lat, addrs.Lng).Scan(&addrs.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 type Addresses []Address
+
+func GetAddresses(db *sql.DB, start, count int) (Addresses, error) {
+	rows, err := db.Query(
+		"SELECT id, address, lat, lng FROM addresses LIMIT $1 OFFSET $2",
+		count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	addresses := Addresses{}
+
+	for rows.Next() {
+		var addrs Address
+		if err := rows.Scan(&addrs.ID, &addrs.Address, &addrs.Lat, &addrs.Lng); err != nil {
+			return nil, err
+		}
+		addresses = append(addresses, addrs)
+	}
+
+	return addresses, nil
+}
