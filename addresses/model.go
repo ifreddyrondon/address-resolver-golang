fb8946c4c7b5152af2db2b55b3135b034017ea42
Freddy Rondon <freddy.rondon@mercadolibre.cl>
2017-07-21 13:21:16 -0400

    refactor: move the queries to constants

--- addresses/model.go
+++ addresses/model.go
@@ -5,6 +5,14 @@ import (
 	"log"
 )
 
+const (
+	GetAddressQuery    = "SELECT address, lat, lng FROM addresses WHERE id=$1"
+	UpdateAddressQuery = "UPDATE addresses SET address=$1, lat=$2, lng=$3 WHERE id=$4"
+	DeleteAddressQuery = "DELETE FROM addresses WHERE id=$1"
+	CreateAddressQuery = "INSERT INTO addresses(address, lat, lng) VALUES($1, $2, $3) RETURNING id"
+	GetAddressesQuery  = "SELECT id, address, lat, lng FROM addresses LIMIT $1 OFFSET $2"
+)
+
 type Address struct {
 	ID      int     `json:"id"`
 	Address string  `json:"address"`
@@ -13,41 +21,31 @@ type Address struct {
 }
 
 func (addrs *Address) getAddress(db *sql.DB) error {
-	return db.QueryRow("SELECT address, lat, lng FROM addresses WHERE id=$1", addrs.ID).Scan(&addrs.Address, &addrs.Lat, &addrs.Lng)
+	return db.QueryRow(GetAddressQuery, addrs.ID).Scan(&addrs.Address, &addrs.Lat, &addrs.Lng)
 }
 
 func (addrs *Address) updateAddress(db *sql.DB) error {
-	_, err :=
-		db.Exec("UPDATE addresses SET address=$1, lat=$2, lng=$3 WHERE id=$4",
-			addrs.Address, addrs.Lat, addrs.Lng, addrs.ID)
-
+	_, err := db.Exec(UpdateAddressQuery, addrs.Address, addrs.Lat, addrs.Lng, addrs.ID)
 	return err
 }
 
 func (addrs *Address) deleteAddress(db *sql.DB) error {
-	_, err := db.Exec("DELETE FROM addresses WHERE id=$1", addrs.ID)
-
+	_, err := db.Exec(DeleteAddressQuery, addrs.ID)
 	return err
 }
 
 func (addrs *Address) createAddress(db *sql.DB) error {
-	err := db.QueryRow(
-		"INSERT INTO addresses(address, lat, lng) VALUES($1, $2, $3) RETURNING id",
-		addrs.Address, addrs.Lat, addrs.Lng).Scan(&addrs.ID)
-
+	err := db.QueryRow(CreateAddressQuery, addrs.Address, addrs.Lat, addrs.Lng).Scan(&addrs.ID)
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
 
 type Addresses []Address
 
 func GetAddresses(db *sql.DB, start, count int) (Addresses, error) {
-	rows, err := db.Query(
-		"SELECT id, address, lat, lng FROM addresses LIMIT $1 OFFSET $2",
-		count, start)
+	rows, err := db.Query(GetAddressesQuery, count, start)
 	if err != nil {
 		return nil, err
 	}
