5383d39e4f33bc603e6626931e0dc3fae1c4171c
Freddy Rondon <freddy.rondon@mercadolibre.cl>
2017-08-19 23:18:23 -0300

    test: mock service client to return especifics responses

---
+++ gmap/export_test.go
@@ -0,0 +1,5 @@
+package gmap
+
+func SetClientInstance(c AddressClient) {
+	serviceInstance.client = c
+}
--- gmap/service_test.go
+++ gmap/service_test.go
@@ -3,10 +3,31 @@ package gmap_test
 import (
 	"testing"
 
+	"net/http"
+
+	"io/ioutil"
+	"net/http/httptest"
+	"path/filepath"
+
 	"github.com/ifreddyrondon/address-resolver-golang/gmap"
+	"github.com/ifreddyrondon/address-resolver-golang/gmap/gmapfakes"
 )
 
+func getClientResponse(fixtureFile string) *http.Response {
+	response := httptest.NewRecorder()
+	fileName := filepath.Join("testdata", fixtureFile)
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	response.Write(content)
+	return response.Result()
+}
+
 func TestGetLatLngFromSimpleAddress(t *testing.T) {
+	client := new(gmapfakes.FakeAddressClient)
+	client.GetGeocodingReturns(getClientResponse("apoquindo_fixture.json"), nil)
+
 	gmapService := gmap.GetService()
 	coordinate, err := gmapService.GetLatLngFromAddress("Apoquindo")
 	if err != nil {
